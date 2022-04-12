8cd74f342dafb7dc86262582cb3012aedd109797
Nishtha Patel <nishthapatel@Nishthas-MacBook-Pro.local>
2022-04-12 16:56:43 -0400

    Achived Restaurant name in a list

--- src/app/add-restaurant/add-restaurant.page.ts
+++ src/app/add-restaurant/add-restaurant.page.ts
@@ -1,31 +1,58 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { RestaurantService } from '../services/restaurant/restaurant.service';
 @Component({
   selector: 'app-add-restaurant',
   templateUrl: './add-restaurant.page.html',
   styleUrls: ['./add-restaurant.page.scss'],
 })
 export class AddRestaurantPage implements OnInit {
-
-  id:any;
-  name:any;
-  address:any;
-  number:any;
-  rating:any;
-  tags:any;
-  comments:any;
-  confirm:any;
-
-  streetNo:any;
-  streetName:any;
-  streetType:any;
-  city:any;
   
-  constructor() { }
+  constructor(
+    private restaurantService: RestaurantService,
+    private router: Router,
+  ) { }
+
+  // id:any;
+  // name:any;
+  // address:any;
+  // number:any;
+  // rating:any;
+  // tags:any;
+  // comments:any;
+  // confirm:any;
+
+  // streetNo:any;
+  // streetName:any;
+  // streetType:any;
+  // city:any;
+  
+  success:string=''
+  error:string=''
+  name:string=''
+
+  description: string=''
+  tags:[]
+
+
+
 
   
-  addRestaurant() {
-
+  async addRestaurant() {
+    this.restaurantService
+    .createRestaurant(
+      this.name,
+      this.description,
+    )
+    .then(() => {
+      this.success = "Logged in";
+      this.error = "";
+      this.router.navigateByUrl('/home')
+    })
+    .catch(err => {
+      this.error = err;
+      this.success = ""
+    });
   
 
 
